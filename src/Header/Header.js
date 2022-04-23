16640e7e8d182f02888368658de42e4592b1ae16
Muhammet Mesut Koç <kocm@THB-ARG-696.home>
2022-04-23 16:10:11 +0300

    Fix header css issues

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,19 +1,15 @@
 import React from "react";
 import "./Header.scss";
 import Button from "./Button";
+import SearchBox from "./SearchBox";
 
 function Header() {
     return (
         <div className="rootHeader">
-            <label className="headerTitle">METLIX</label>
-            <div>
-                <input className="headerInput" placeholder="Search any movie"></input>
-                <button className="headerSearchButton">Search</button>
-            </div>
-            <div>
-                <Button type={"Genres"}></Button>
-                <Button type={"Basket"}></Button>
-            </div>
+            <label className="headerTitle">METFLIX</label>
+            <SearchBox></SearchBox>
+            <Button type={"Genres"}></Button>
+            <Button type={"Basket"}></Button>
         </div>);
 }
 export default Header;
---
+++ src/Header/SearchBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchBox() {
+    return (
+        <div className="searchbox">
+            <input className="headerInput" placeholder="Search any movie"></input>
+            <button className="headerSearchButton">Search</button>
+        </div>);
+}
+
+export default SearchBox;
