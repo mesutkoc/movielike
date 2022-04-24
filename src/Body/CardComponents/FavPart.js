8d9c8c2da3e8957d0728132bf8dce096186e43cd
Muhammet Mesut Koç <kocm@THB-ARG-696.home>
2022-04-24 17:20:21 +0300

    Add icons and useraction part as like play or save

---
+++ src/Body/CardComponents/FavPart.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./FavPart.scss"
+import {ReactComponent as CirclePlay} from "../../Icons/circle-play.svg"
+import {ReactComponent as Star} from "../../Icons/star-solid.svg"
+import {ReactComponent as Heart} from "../../Icons/heart-solid.svg"
+import {ReactComponent as Eye} from "../../Icons/eye-solid.svg"
+
+function FavPart() {
+  return (<div className="favItems">
+      
+        <div className="userAction">
+            <CirclePlay className="playIcon"></CirclePlay>
+            <Star className="star"></Star>
+            <Heart className="heart"></Heart>
+        </div>
+        <div className="viewCount">
+            <Eye className="eye"></Eye>
+            <label className="count">9999</label>
+        </div>
+      </div>);
+}
+
+export default FavPart;
--- src/Body/Horizontal/HorizontalCard.js
+++ src/Body/Horizontal/HorizontalCard.js
@@ -5,6 +5,7 @@ import CardImage from "../CardComponents/CardImage";
 import CardHeader from "../CardComponents/CardHeader";
 import CardOverview from "../CardComponents/CardOverview";
 import TrailerButton from "../CardComponents/TrailerButton";
+import FavPart from "../CardComponents/FavPart";
 
 function HorizontalCard() {
   const { movie } = useMovie();
@@ -17,6 +18,9 @@ function HorizontalCard() {
       <CardOverview overview={movieData?.overview}></CardOverview>
       <div className="trailerInfo">
         <TrailerButton></TrailerButton>
+      </div>
+      <div className="favPart">
+        <FavPart></FavPart>
       </div>
   </div>;
 }
