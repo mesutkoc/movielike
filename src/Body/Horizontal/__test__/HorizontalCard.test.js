44000a0d5a42c4f5cc5cc5ec422ac7a5e271f03b
Muhammet Mesut Koç <kocm@THB-ARG-696.local>
2022-05-21 17:18:27 +0300

    Add unit test to genres component with props

--- src/Body/Horizontal/__test__/HorizontalCard.test.js
+++ src/Body/Horizontal/__test__/HorizontalCard.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {render} from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { mount } from "enzyme";
 import HorizontalCard from "../HorizontalCard";
 import CardImage from "../../CardComponents/CardImage"
@@ -8,11 +8,11 @@ import CardOverview from "../../CardComponents/CardOverview"
 import TrailerButton from "../../CardComponents/TrailerButton"
 import FavPart from "../../CardComponents/FavPart"
 import Genres from "../../CardComponents/Genres"
-import {horizantalCardData} from "./horizantalCardDummyData"
+import { horizantalCardData } from "./horizantalCardDummyData"
 
-describe('Horizontal Card test',()=>{
+describe('Horizontal Card test', () => {
 
-    it('should render 6 components',()=>{
+    it('should render 6 components', () => {
         const wrapper = mount(<HorizontalCard></HorizontalCard>);
         expect(wrapper.containsMatchingElement(<CardImage></CardImage>)).toBe(true);
         expect(wrapper.containsMatchingElement(<CardHeader></CardHeader>)).toBe(true);
@@ -21,14 +21,25 @@ describe('Horizontal Card test',()=>{
         expect(wrapper.containsMatchingElement(<FavPart></FavPart>)).toBe(true);
         expect(wrapper.containsMatchingElement(<Genres></Genres>)).toBe(true);
     })
-    
-    it('Rendering with props',()=>{
-        const wrapper = mount(<HorizontalCard movieData={horizantalCardData} index={0}/>)
+
+    it('Rendering with props', () => {
+        const wrapper = mount(<HorizontalCard movieData={horizantalCardData} index={0} />)
         expect(wrapper.containsAllMatchingElements([<div className="cardHeader"><label className="cardTitle">Shazam!</label></div>,
         <div className="cardOverview"><label className="overview">A boy is given the ability to become an adult superhero in times of need with a single magic word.</label></div>,
         <div key={0} className="genreType">Action</div>,
         <div key={1} className="genreType">Comedy</div>,
-        <div key={2} className="genreType">Fantasy</div>]))
-        .toBe(true);
+        <div key={2} className="genreType">Fantasy</div>
+        ])).toBe(true);
+    })
+    
+    it('Rendering Genres comp with props', () => {
+        const wrapper = mount(<Genres genres={horizantalCardData?.genres} />)
+        expect(wrapper.containsMatchingElement(
+            <div className="genresTable">
+                <div key={0} className="genreType">Action</div>
+                <div key={1} className="genreType">Comedy</div>
+                <div key={2} className="genreType">Fantasy</div>
+            </div>
+        )).toBe(true)
     })
 })
