82a148d645f27364eae2defa130de7ded22d49b6
Muhammet Mesut Koç <mestkoc34@gmail.com>
2022-06-19 15:52:40 +0300

    Update horizontal card

--- src/Body/HorizontalCard/HorizontalCard.js
+++ src/Body/HorizontalCard/HorizontalCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import HorizontalCardImage from "../HorizontalCardComponents/HorizontalCardImage";
-import HorizontalCardTitle from "../HorizontalCardComponents/HorizontalCardTitle";
 import CardOverview from "../HorizontalCardComponents/CardOverview";
 import TrailerButton from "../HorizontalCardComponents/TrailerButton";
 import HorizontalCardFavPart from "../HorizontalCardComponents/HorizontalCardFavPart";
@@ -11,7 +10,6 @@ function HorizontalCard({movieData}) {
   return <div className="horizontalCard">
       <HorizontalCardImage image={movieData?.poster}></HorizontalCardImage>
       <div className="horizontalCardRightSide">
-           <HorizontalCardTitle title={movieData?.title}></HorizontalCardTitle>
            <CardOverview overview={movieData?.overview}></CardOverview>
            <TrailerButton id={movieData?.id}></TrailerButton>
            <HorizontalCardFavPart></HorizontalCardFavPart>
--- src/Body/HorizontalCard/__test__/HorizontalCard.test.js
+++ src/Body/HorizontalCard/__test__/HorizontalCard.test.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { mount } from "enzyme";
 import HorizontalCard from "../HorizontalCard";
 import HorizontalCardImage from "../../HorizontalCardComponents/HorizontalCardImage"
-import HorizontalCardTitle from "../../HorizontalCardComponents/HorizontalCardTitle"
 import CardOverview from "../../HorizontalCardComponents/CardOverview"
 import TrailerButton from "../../HorizontalCardComponents/TrailerButton"
 import HorizontalCardFavPart from "../../HorizontalCardComponents/HorizontalCardFavPart"
@@ -16,7 +15,6 @@ describe('Horizontal Card test', () => {
     it('should render 6 components', () => {
         const wrapper = mount(<BrowserRouter><HorizontalCard></HorizontalCard></BrowserRouter>);
         expect(wrapper.containsMatchingElement(<HorizontalCardImage></HorizontalCardImage>)).toBe(true);
-        expect(wrapper.containsMatchingElement(<HorizontalCardTitle></HorizontalCardTitle>)).toBe(true);
         expect(wrapper.containsMatchingElement(<CardOverview></CardOverview>)).toBe(true);
         expect(wrapper.containsMatchingElement(<TrailerButton></TrailerButton>)).toBe(true);
         expect(wrapper.containsMatchingElement(<HorizontalCardFavPart></HorizontalCardFavPart>)).toBe(true);
@@ -25,15 +23,15 @@ describe('Horizontal Card test', () => {
 
     it('Rendering with props', () => {
         const wrapper = mount(<BrowserRouter><HorizontalCard movieData={horizantalCardData} index={0} /></BrowserRouter>)
-        expect(wrapper.containsAllMatchingElements([<div className="cardHeaders"><label className="cardTitles">Shazam!</label></div>,
-        <div className="cardImages"><img className="imageSizes" src={horizantalCardData?.poster} alt="error"></img></div>,
-        <div className="cardOverview"><label className="overview">A boy is given the ability to become an adult superhero in times of need with a single magic word.</label></div>,
-        <div key={0} className="genreType">Action</div>,
-        <div key={1} className="genreType">Comedy</div>,
-        <div key={2} className="genreType">Fantasy</div>
+        expect(wrapper.containsAllMatchingElements([
+            <div className="cardImages"><img className="imageSizes" src={horizantalCardData?.poster} alt="error"></img></div>,
+            <div className="cardOverview"><label className="overview">A boy is given the ability to become an adult superhero in times of need with a single magic word.</label></div>,
+            <div key={0} className="genreType">Action</div>,
+            <div key={1} className="genreType">Comedy</div>,
+            <div key={2} className="genreType">Fantasy</div>
         ])).toBe(true);
     })
-    
+
     it('Rendering Genres comp with props', () => {
         const wrapper = mount(<Genres genres={horizantalCardData?.genres} />)
         expect(wrapper.containsMatchingElement(
