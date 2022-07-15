e257a2adcbe7df48a13589c72cd131ba6facf8e2
Muhammet Mesut Koç <mestkoc34@gmail.com>
2022-07-16 00:50:43 +0300

    create type constant file and update styles

--- src/Body/Category/CategoryCard.js
+++ src/Body/Category/CategoryCard.js
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 import "./CategoryCard.scss";
 
 function CategoryCard({ genreItem }) {
-    return  <Link to={`category/${genreItem?.genre}`}><div className="catCard">
+    return <Link to={`category/${genreItem?.genre}`} className="categoryLink">
+                <div className="catCard">
                 <div className="catCardImages"><img className="catImageSizes" src={genreItem?.poster} alt="error"></img></div>
                     <p>{genreItem?.genre}</p>
                 </div>
--- src/Body/HorizontalCardComponents/TrailerButton.js
+++ src/Body/HorizontalCardComponents/TrailerButton.js
@@ -10,7 +10,7 @@ function TrailerButton({id}) {
     return (
         <div className="trailerButton">
             <Link to={`details/${id}`} className="buttonLink"><button className="button">SEE DETAILS</button></Link>
-            <div className="bigCircle" style={{ backgroundImage: `conic-gradient(red 0deg, red ${points[1] * 10}deg,black 0deg, black 360deg)` }}>
+            <div className="bigCircle" style={{ backgroundImage: `conic-gradient(#FFA500 0deg, #FFA500 ${points[1] * 10}deg,black 0deg, black 360deg)` }}>
                 <div className="middleCircle">{points[0]}</div>
             </div>
         </div>);
