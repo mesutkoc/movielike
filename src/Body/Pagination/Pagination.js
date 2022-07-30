768d7b2712fd7932c87dbb6f6f5e38c8888d67ed
Muhammet Mesut Koç <mestkoc34@gmail.com>
2022-07-30 23:27:24 +0300

    add page number on url with pagination items

--- src/Body/Pagination/Pagination.js
+++ src/Body/Pagination/Pagination.js
@@ -17,12 +17,12 @@ function Pagination({ pagenumber, handleClick, currentPage, setCurrentPage, genr
         }
 
     }
-
+    console.log(pagenumber);
     return <div className="pagination">
         <Link to={`/${Routes_Const.CATEGORY}/${genre}&${Routes_Const.PAGE}=${currentPage}`}><div className="prev" onClick={() => selectPage("prev")}><span>-</span></div></Link>
-        <div className="paginationList">
+        <Link to={`/${Routes_Const.CATEGORY}/${genre}&${Routes_Const.PAGE}=${currentPage}`} className="paginationList">
             {pagenumber.map((item, index) => <span className={`paginationListItem${item === currentPage ? "Active" : ""}`} key={index + 1} onClick={() => handleClick(item)}>{item}</span>)}
-        </div>
+        </Link>
         <Link to={`/${Routes_Const.CATEGORY}/${genre}&${Routes_Const.PAGE}=${currentPage}`}><div className="next" onClick={() => selectPage('next')}><span>+</span></div></Link>
     </div>;
 }
