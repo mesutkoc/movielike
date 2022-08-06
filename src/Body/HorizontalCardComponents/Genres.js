902a8e82836339e9bc0b09af249c0dfb61f786fb
Muhammet Mesut Koç <mestkoc34@gmail.com>
2022-08-07 00:22:00 +0300

    Update: showing genre types on card

--- src/Body/HorizontalCardComponents/Genres.js
+++ src/Body/HorizontalCardComponents/Genres.js
@@ -3,10 +3,8 @@ import "./Genres.scss"
 
 function Genres({genres}) {
   return (<div className="genresTable">
-      
-      {genres?.slice(0,3).map((genre,index)=><div key={index} className="genreType">{genre}</div>)}
-      
-      </div>);
+      {genres?.map((genre,index)=><div key={index} className="genreType">{genre}</div>)}
+    </div>);
 }
 
 export default Genres;
