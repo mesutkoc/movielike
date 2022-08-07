e6aadd5dfc126d885ae743dea8d188415f316842
Muhammet Mesut Koç <mestkoc34@gmail.com>
2022-08-07 22:08:46 +0300

    Add: search result unit test 2

--- src/Body/SearchResult/__test__/SearchResult.test.js
+++ src/Body/SearchResult/__test__/SearchResult.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import SearchResult from "../SearchResult";
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 
 describe('Search result test', () => {
     const dummyData = {count: 100, genre: 'Comedy'}
@@ -9,4 +9,9 @@ describe('Search result test', () => {
         const wrapper = mount(<SearchResult searchResult={dummyData.count} genre={dummyData.genre}></SearchResult>);
         expect(wrapper.containsMatchingElement(<div className="searchResult">Found<span className="searchResSpan">{dummyData.count}</span>movies related to<span className="searchResSpan">{dummyData.genre}</span>category.</div>)).toBe(true);
     })
+
+    it('renders three search result components', () => {
+        const wrapper = shallow(<SearchResult />);
+        expect(wrapper).toMatchSnapshot();
+      });
 })
