2fd9eaa22d6594520db4856d03a1e0025752c446
Muhammet Mesut Koç <mestkoc34@gmail.com>
2022-09-04 01:26:28 +0300

    update: header and searchbox

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,7 +7,8 @@ import SearchBox from "./SearchBox";
 function Header({ setGenreFiltersView, showGenreFilters, showBasket, setBasketView, basket }) {
   return (
     <div className="rootHeader">
-      <Link to="/" className="headerTitlelink">MOVIELIKE</Link>
+      
+      <Link to="/" className="headerTitlelink"><div className="logo"></div>MOVIELIKE</Link>
       <SearchBox></SearchBox>
       <Button type={"Basket"} setGenreFiltersView={setGenreFiltersView} showGenreFilters={showGenreFilters} showBasket={showBasket} setBasketView={setBasketView} basket={basket}></Button>
     </div>);
--- src/Header/SearchBox.js
+++ src/Header/SearchBox.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SearchBox() {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const keyDownHandler = (event) => {
+
+        if (event.key === 'Enter') {
+            console.log('enter', searchTerm);
+        }
+    }
+
     return (
         <div className="searchbox">
-            <input className="headerInput" placeholder="Search any movie"></input>
-            <button className="headerSearchButton">Search</button>
+            <input className="headerInput"
+                placeholder="Search any movie" id="message"
+                name="message"
+                onChange={event => setSearchTerm(event.target.value)}
+                onKeyDown={keyDownHandler}></input>
         </div>);
 }
 
