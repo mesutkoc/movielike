e1e0e715e0c3f8acdfb343d3ed555af5dd4c7570
Muhammet Mesut Koç <mestkoc34@gmail.com>
2022-09-10 22:59:28 +0300

    Add: pass recomovie data

--- src/Body/WatchPart/RecommendedPart.js
+++ src/Body/WatchPart/RecommendedPart.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./WatchPart.scss";
 
-function RecommendedPart({fullscreen}) {
+function RecommendedPart({ fullscreen, recoMovie }) {
+  console.log(recoMovie);
   return <div className={`recommendPart ${fullscreen}`}>RecommendedPart</div>;
 }
 
--- src/Body/WatchPart/WatchMovie.js
+++ src/Body/WatchPart/WatchMovie.js
@@ -1,23 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useParams } from "react-router-dom";
 import { getMovieByID } from "../../Requests/MovieRequest";
 import RecommendedPart from "./RecommendedPart";
 import Screen from "./Screen";
 
-function WatchMovie() {
+function WatchMovie({ movies }) {
     const [movie, setMovie] = useState({});
     const [fullscreen, setScreen] = useState(true);
 
     const movieItem = useParams();
-    
+
     useEffect(() => {
         getMovieByID(movieItem.id).then(res => setMovie(res));
     }, [movieItem.id])
 
+    const recoMovie = useMemo(() => (
+        movies?.filter(item => item?.genres?.find(genre => movie?.genres?.[0] === genre && movie?.title !== item?.title))?.slice(0,15)
+    ), [movies, movie])
 
-    return <div className="watchPart"> 
+    return <div className="watchPart">
         <Screen movie={movie} setScreen={setScreen} fullscreen={fullscreen}></Screen>
-        <RecommendedPart fullscreen={fullscreen}></RecommendedPart>
+        <RecommendedPart fullscreen={fullscreen} recoMovie={recoMovie}></RecommendedPart>
     </div>;
 }
 
