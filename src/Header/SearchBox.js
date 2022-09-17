16c6a06b4edec95f8d842d804303c2a8a645d25c
Muhammet Mesut Koç <mestkoc34@gmail.com>
2022-09-18 00:48:58 +0300

    add: search movie and style suggestionbox

--- src/Header/SearchBox.js
+++ src/Header/SearchBox.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import SuggestionPart from "./SuggestionPart";
+import { useMovie } from "../Context/MovieContext";
 
 function SearchBox() {
     const [searchTerm, setSearchTerm] = useState('');
+    const [suggestionMovie, setSuggestionMovie] = useState([]);
     const [focused, setFocus] = useState(false);
+    const { movies } = useMovie();
 
     const keyDownHandler = (event) => {
+        const suggestMovies = movies?.filter(item => item?.title?.toLowerCase().trim().includes(searchTerm));
+        searchTerm?.length > 1 ? setSuggestionMovie(suggestMovies) : setSuggestionMovie([]);
 
         if (event.key === 'Enter') {
             console.log('enter', searchTerm);
@@ -13,11 +18,11 @@ function SearchBox() {
     }
 
     const onFocus = (event) => {
-        setFocus(true)
+        setFocus(true);
     }
 
     const onBlur = (event) => {
-        setFocus(false)
+        setFocus(false);
     }
 
     return (
@@ -27,12 +32,13 @@ function SearchBox() {
                 onBlur={onBlur}
                 placeholder="Search any movie" id="message"
                 name="message"
-                onChange={event => setSearchTerm(event.target.value)}
+                autoComplete="off"
+                onChange={event => setSearchTerm(event.target.value.toLowerCase().trim())}
                 onKeyDown={keyDownHandler}></input>
-                {
-                    focused && <SuggestionPart></SuggestionPart>
-                }           
-                
+            {
+                focused && <SuggestionPart suggestionMovie={suggestionMovie}></SuggestionPart>
+            }
+
         </div>);
 }
 
--- src/Header/SuggestionPart.js
+++ src/Header/SuggestionPart.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Routes_Const } from "../constants";
 import "./SuggestionPart.scss"
 
-function SuggestionPart() {
-  return <div className="suggestionPart">SuggestionPart</div>;
+function SuggestionPart({ suggestionMovie }) {
+  return <div className="suggestionPart">
+    <span className="suggestionMessage"><p>Type <strong> at least 2 </strong> characters.</p> </span>
+    {suggestionMovie?.map((movie, index) =>
+      <Link to={`/${Routes_Const.DETAILS}/${movie?.id}`} key={index}><p className="suggestItem" >{movie?.title}</p></Link>)}
+  </div>;
 }
 
 export default SuggestionPart;
