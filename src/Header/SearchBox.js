258515a54c0146b41c6a984fec0add709bc6fe64
Muhammet Mesut Koç <mestkoc34@gmail.com>
2022-10-17 00:29:39 +0300

    add: search box icon and style

--- src/Header/SearchBox.js
+++ src/Header/SearchBox.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useMovie } from "../Context/MovieContext";
 import { useNavigate } from 'react-router-dom';
 import SuggestionPart from "./SuggestionPart";
+import { ReactComponent as SearchIcon } from "../Icons/search-icon.svg"
 import { Routes_Const } from "../constants";
 
 function SearchBox() {
@@ -16,7 +17,7 @@ function SearchBox() {
     const keyDownHandler = (event) => {
         const suggestMovies = movies?.filter(item => item?.title?.toLowerCase().trim().includes(searchTerm));
         searchTermLength > 1 ? setSuggestionMovie(suggestMovies) : setSuggestionMovie([]);
-        
+
         if (event.key === 'Enter' && searchTermLength > 1) {
             navigate(`${Routes_Const.SEARCH_RESULT}`, { state: suggestMovies });
             setFocus(false);
@@ -34,18 +35,20 @@ function SearchBox() {
 
     return (
         <div className="searchbox">
-            <input className={`headerInput ${focused}`}
-                onFocus={onFocus}
-                onBlur={onBlur}
-                placeholder="Search any movie" id="message"
-                name="message"
-                autoComplete="off"
-                onChange={event => setSearchTerm(event.target.value.toLowerCase().trim())}
-                onKeyDown={keyDownHandler}></input>
+            <div className={`searchInput ${focused}`}>
+                <SearchIcon className="searchIcon"></SearchIcon>
+                <input className={`headerInput`}
+                    onFocus={onFocus}
+                    onBlur={onBlur}
+                    placeholder="Search any movie" id="message"
+                    name="message"
+                    autoComplete="off"
+                    onChange={event => setSearchTerm(event.target.value.toLowerCase().trim())}
+                    onKeyDown={keyDownHandler}></input>
+            </div>
             {
                 focused && <SuggestionPart suggestionMovie={suggestionMovie} searchTerm={searchTerm}></SuggestionPart>
             }
-
         </div>);
 }
 
